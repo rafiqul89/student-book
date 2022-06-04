4ff3d50c4ae55316154c103c3619ec842ca543a0
Rafiqul89 <mrirafiq898@gmail.com>
2022-06-04 20:46:40 +0600

    first commit

--- index.php
+++ index.php
@@ -67,7 +67,6 @@ require('dbcon.php'); ?>
           </div>
       </div>
   </div>
-<p>sdf</p>
 
    <!-- Option 1: Bootstrap Bundle with Popper -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
